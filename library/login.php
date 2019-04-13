60f998338b8351b9ae5b7c151cef56a11d17d5d5
wuwei87 <1249522631@qq.com>
2019-04-13 14:45:22 +0800

    19-4-13

--- library/login.php
+++ library/login.php
@@ -1,5 +1,17 @@
 <?php 
-$username = $_POST["loginName"];
+/* $username = $_POST["loginName"];
 $userpass = $_POST["loginPass"];
 echo $username."   ".$userpass;
+ */
+$DOCUMENT_ROOT=$_SERVER['DOCUMENT_ROOT'];
+$fp=fopen("$DOCUMENT_ROOT/a.txt",'ab');
+$putstring="xiewenjian";
+fwrite($fp,$putstring,strlen($putstring));
+if(!$fp)
+{
+	echo "<p><strong>Your file could not be processed at this time.Please try again later.</strong></p>";
+	echo "$DOCUMENT_ROOT";
+	exit;
+}
+fclose($fp);
 ?>
